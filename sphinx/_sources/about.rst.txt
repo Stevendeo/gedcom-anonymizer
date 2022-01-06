1eeaf7987779f1e19dea47bb692c495500f299f9
lefessan <fabrice@lefessant.net>
2022-01-06 13:05:47 +0000

    Deploying to gh-pages from  @ f488e64708fa7e720a1369a23c94ff2b9fb594d1 🚀

---
+++ sphinx/_sources/about.rst.txt
@@ -0,0 +1,11 @@
+About
+=====
+
+This is the description
+of the gedcom-anonymizer OCaml project
+
+
+Authors
+-------
+
+* Fabrice Le Fessant <fabrice.le_fessant@origin-labs.com>
---
+++ sphinx/_static/documentation_options.js
@@ -0,0 +1,10 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: 'master (f488e64) (2022/01/06 13:05)',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    FILE_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+};
