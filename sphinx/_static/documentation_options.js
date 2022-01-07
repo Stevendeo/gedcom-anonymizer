bfc9635c25784a1c76297df9a6d1cf08b167a1ff
Stevendeo <de.oliveira.steven@gmail.com>
2022-01-07 13:08:42 +0000

    Deploying to gh-pages from  @ a0b70c73bee3ee4329bbe7db67a39e48772d4260 🚀

--- sphinx/_static/documentation_options.js
+++ sphinx/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'master (acdf0dc) (2022/01/06 15:16)',
+    VERSION: 'master (a0b70c7) (2022/01/07 13:08)',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
